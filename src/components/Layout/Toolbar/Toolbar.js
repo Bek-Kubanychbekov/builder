0de0b174d0333cc99608401cdbf731ce0b87cfc7
Kubanychbekov Bek <kbnv001@gmail.com>
2021-03-17 16:52:59 +0600

    Added in Drawer, Toolbar

--- src/components/Layout/Toolbar/Toolbar.js
+++ src/components/Layout/Toolbar/Toolbar.js
@@ -1,6 +1,6 @@
 import classes from "./Toolbar.module.css";
-import Logo from "../Logo/Logo.js";
-import Nav from "../Nav/Nav.js";
+import Logo from "../../Logo/Logo.js";
+import Nav from "../../Nav/Nav.js";
 const Toolbar = () => {
     return (
         <div className={classes.Toolbar}>

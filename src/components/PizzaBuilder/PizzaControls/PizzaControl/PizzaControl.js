c74a5b36d86942cd4c13b261a77659c05af39825
Kubanychbekov Bek <kbnv001@gmail.com>
2021-04-09 16:07:41 +0600

    ingredients

--- src/components/PizzaBuilder/PizzaControls/PizzaControl/PizzaControl.js
+++ src/components/PizzaBuilder/PizzaControls/PizzaControl/PizzaControl.js
@@ -3,11 +3,20 @@ import PizzaIngredient from "../../PizzaIngredient/PizzaIngredient";
 import classes from "./PizzaControl.module.css";
 
 const PizzaControl = ({ type, add, remove, count }) => {
+  const name ={
+    tomato: "Cucumber" ,
+    salami: "Salad",
+    greenOlive: "Fries",
+    blackOlive: "Meat",
+    redPepper: "Ketchup",
+    yellowPepper: "Tomato",
+  }
   return (
     <div className={classes.PizzaControl}>
       <Button onClick={() => add(type)}>+</Button>
       <div className={classes.ingredient}>
-        <PizzaIngredient type={type} fixed />
+        {/* <PizzaIngredient type={type} fixed /> */}
+        {name[type]}
       </div>
       <Button onClick={() => remove(type)} disabled={!count}>-</Button>
     </div>
--- src/components/PizzaBuilder/PizzaPreview/PizzaPreview.js
+++ src/components/PizzaBuilder/PizzaPreview/PizzaPreview.js
@@ -1,7 +1,7 @@
 import PizzaIngredient from "../PizzaIngredient/PizzaIngredient";
 
 import classes from "./PizzaPreview.module.css";
-import ingredientsBackground from "../../../images/tomato.svg";
+import ingredientsBackground from "../../../images/cheese.svg";
 
 const PizzaPreview = ({ ingredients, price }) => {
   const result = [];

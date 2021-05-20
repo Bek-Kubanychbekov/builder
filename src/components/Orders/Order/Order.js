55ceb0efc02c7eed63348d191367c39fcee83a90
Kubanychbekov Bek <kbnv001@gmail.com>
2021-05-20 21:34:29 +0600

    Add orders

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -1,9 +1,17 @@
 import classes from "./Order.module.css";
 
 const Order = ({ name, phone, address, flowers,price }) => {
-  const output = Object.keys(flowers).map(label => <em>{label}:{flowers [label]} </em>)
+  const output = Object.keys(flowers).map(label => <em>{label}: - {flowers[label]}</em>)
+
+
   return (
     <ul className={classes.Order}>
+      <li>Name:<strong>{name}</strong> </li>
+      <li>Phone:<strong>{phone}</strong> </li>
+      <li>Address:<strong>{address}</strong> </li>
+      <li >Flowers bouquets house: {output}</li>
+      <li className={classes.som} >the buy som :{price}</li>
+      <li><strong>Thanks for your purchase</strong></li>  
       <li>Name:<strong> {name}</strong> </li>
       <li>Phone:<strong> {phone}</strong> </li>
       <li>Address:<strong> {address}</strong> </li>
@@ -12,8 +20,8 @@ const Order = ({ name, phone, address, flowers,price }) => {
       <li><strong>Thanks for your purchase !!!</strong></li>  
        <div>@Bek</div>
     </ul>
- 
-  );
+
+  );  
 }
 
 export default Order;

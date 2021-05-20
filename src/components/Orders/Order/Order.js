30e73313c756a066dd7a08e42f1ad86fdb0b697a
Kubanychbekov Bek <kbnv001@gmail.com>
2021-05-20 21:45:50 +0600

    Add new orders components

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -5,13 +5,7 @@ const Order = ({ name, phone, address, flowers,price }) => {
 
 
   return (
-    <ul className={classes.Order}>
-      <li>Name:<strong>{name}</strong> </li>
-      <li>Phone:<strong>{phone}</strong> </li>
-      <li>Address:<strong>{address}</strong> </li>
-      <li >Flowers bouquets house: {output}</li>
-      <li className={classes.som} >the buy som :{price}</li>
-      <li><strong>Thanks for your purchase</strong></li>  
+    <ul className={classes.Order}> 
       <li>Name:<strong> {name}</strong> </li>
       <li>Phone:<strong> {phone}</strong> </li>
       <li>Address:<strong> {address}</strong> </li>

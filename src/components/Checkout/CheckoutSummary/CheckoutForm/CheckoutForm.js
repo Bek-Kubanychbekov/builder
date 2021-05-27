9d99d18454cb9437164af6d8cbf5e43f1211c37a
Kubanychbekov Bek <kbnv001@gmail.com>
2021-05-27 15:03:33 +0600

    Fixed some sstuff

--- src/components/Checkout/CheckoutSummary/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutSummary/CheckoutForm/CheckoutForm.js
@@ -4,15 +4,15 @@ import classes from "./CheckoutForm.module.css"
 const CheckoutForm = ({submitCallback, cancelCallback }) => {
     return (  <form className={classes.CheckoutForm} onSubmit={submitCallback}>
         <div>
-        <label for="name" >Name</label>
+        <label htmlFor="name" >Name</label>
         <input type="text" id="name" name="name" required placeholder="EX: Elon Musk"></input>
         </div>
         <div>
-        <label for="phone">Phone</label>
+        <label htmlFor="phone">Phone</label>
         <input type="tel" id="phone" name="phone" placeholder="EX: 0500123456" required pattern="0[0-9]{9}"></input>
         </div>
         <div>
-        <label for="address">Address</label>
+        <label htmlFor="address">Address</label>
         <input type="text" id="address" name="address" required placeholder="EX: № 31 ctrit Bapa"></input>
         </div>
         
